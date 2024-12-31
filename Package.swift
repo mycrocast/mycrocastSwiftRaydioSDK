8e7e45125fb0c2517ed52040f03033adc5175186
sven-mycrocast <67374475+sven-mycrocast@users.noreply.github.com>
2024-12-31 17:45:42 +0100

    add SPM support

---
+++ Package.swift
@@ -0,0 +1,36 @@
+// swift-tools-version:5.4
+import PackageDescription
+
+let package = Package(
+    name: "SwiftRaydioSDK",
+    platforms: [
+        .iOS(.v14)
+    ],
+    products: [
+        .library(
+            name: "SwiftRaydioSDK",
+            targets: ["SwiftRaydioSDK"]
+        )
+    ],
+    dependencies: [
+        .package(
+            name: "Socket.IO-Client-Swift",
+            url: "https://github.com/socketio/socket.io-client-swift",
+            from: "16.0.0"
+        )
+    ],
+    targets: [
+        .binaryTarget(
+            name: "SwiftRaydioSDK",
+            path: "./SwiftRaydioSDK.xcframework"
+        ),
+        .binaryTarget(
+            name: "Mediasoup",
+            path: "./deps/Mediasoup.xcframework"
+        ),
+        .binaryTarget(
+            name: "WebRTC",
+            path: "./deps/WebRTC.xcframework"
+        )
+    ]
+)
