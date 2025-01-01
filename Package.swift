f4d3f3c239535d84976dc0cb3fd4f016c5e809e9
sven-mycrocast <67374475+sven-mycrocast@users.noreply.github.com>
2025-01-01 14:35:40 +0100

    update path

--- Package.swift
+++ Package.swift
@@ -24,7 +24,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "SwiftRaydioSDK",
-            path: "./SwiftRaydioSDK.xcframework"
+            path: "SwiftRaydioSDK.xcframework"
         )
     ]
 )
